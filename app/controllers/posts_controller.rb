103fc04725c672688a3e98c48da3079e51f64854
VacantFanatic <agill084@gmail.com>
2013-09-19 17:58:11 -0400

    Fixed tutorial bugs in edit and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,10 +22,27 @@ class PostsController < ApplicationController
 
 	def edit
 		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end	
 
 	private
+	
 	def post_params
 		params.require(:post).permit(:title, :text)
 	end
+
+	def update_params
+		
+	end
+		
+
 end
